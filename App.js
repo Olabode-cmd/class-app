5c47641941882cb7b8047c875b659546cada72d6
Olabode-cmd <olabodebalogun80@gmail.com>
2025-04-30 15:23:22 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useState } from 'react'
 
 export default function App() {
+  const [temperature, setTemperature] = useState(20)
+  const [message, setMessage] = useState('Waiting for temperature...')
+
+  const checkTemperature = () => {
+    if (temperature < 25) { 
+      setMessage('It is cold')
+    } else {
+      setMessage('It is hot')
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 50, textAlign: 'center'}}>{temperature}</Text>
+      <Text>{message}</Text>
+
+      <Pressable onPress={checkTemperature}>
+        <Text>Check temperature</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
