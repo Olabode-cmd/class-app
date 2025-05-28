67a8f6de801c9301362bce4e0db363be2431ea78
Olabode-cmd <olabodebalogun80@gmail.com>
2025-05-28 16:34:34 +0100

    fetching get request class examples

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,59 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import React from 'react';
+import { useState, useEffect } from 'react'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+
+  // Json - Javascript Object Notation
+
+  const fetchProducts = async () => { // asynchronous function
+    try {
+      const response = await fetch('https://dummyjson.com/products')
+      if (!response.ok) {
+        throw new Error('Something went wrong. Try again later.')
+      }
+
+      const data = await response.json()
+      // console.log(data)
+      setProducts(data.products)
+    } catch (error) {
+      console.error(error)
+      setError(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    )
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{error}</Text>
+      </View>
+    )
+  }
+
+  // fetchProducts();
+  // setUser("Michael")
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      {products.map((product) => (
+        <Text key={product.id} style={styles.text}>{product.title}</Text>
+      ))}
     </View>
   );
 };
@@ -23,3 +72,11 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen; 
+
+// GET, POST, PUT, PATCH, DELETE
+
+// const user = { name: "Olabode One", course: "Frontend", email: "olabode@google.com"}
+
+// PUT REQUEST = { name: "Olabode One", course: "Backend", email: "olabode@google.com"}
+
+// PATCH REQUEST = { course: "Backend" }
